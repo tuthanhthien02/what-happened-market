3c3539c282cee3dd3ac070383d829125097f2949
Thanh Thien <tuthanhthien02@gmail.com>
2022-01-31 08:34:27 +0700

    adding local fonts, basic folders and files structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1 className="heading-1">Hello there</h1>
+            <h1 className="heading-2">Hello there</h1>
+            <h1 className="heading-3">Hello there</h1>
+            <h1 className="heading-4">Hello there</h1>
+            <h1 className="heading-5">Hello there</h1>
+            <h1 className="heading-6">Hello there</h1>
+        </div>
+    );
 }
 
 export default App;
